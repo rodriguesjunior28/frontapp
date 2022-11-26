177d2302ee6f321f7926272b1dace78cec34c346
Squadrão 17 <111467580+Squad017@users.noreply.github.com>
2022-11-26 02:50:02 -0300

    Iniciando App

---
+++ projetomodelo-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootswatch/dist/solar/bootstrap.css';
+
+function App() {
+  return (
+    <div>
+      Junior Rodrigues - Tudo posso naquele que me fortalece.
+    </div>
+  );
+}
+
+export default App;
+
+// Usando o JSX ele entende os atalhos, por exemplo, se vc colcoar h1 ele entende que é um html
+// Já no JS ele não entende e não vai te dá sugestão. E também é bom usar quando for trabalhar em arquivo que use pouco HTML
