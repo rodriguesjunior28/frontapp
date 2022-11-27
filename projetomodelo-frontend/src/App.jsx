2cda5e93f915d50a5cef72910cbd42783f35fce6
Squadrão 17 <111467580+Squad017@users.noreply.github.com>
2022-11-26 22:28:44 -0300

    Inserindo views

--- projetomodelo-frontend/src/App.jsx
+++ projetomodelo-frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import 'bootswatch/dist/solar/bootstrap.css';
+import CadastroUsuario from './views/cadastroUsuario';
+
 
 function App() {
   return (
     <div>
-      Junior Rodrigues germano meu irmão
+      <CadastroUsuario />
     </div>
   );
 }
@@ -12,3 +14,4 @@ export default App;
 
 // Usando o JSX ele entende os atalhos, por exemplo, se vc colcoar h1 ele entende que é um html
 // Já no JS ele não entende e não vai te dá sugestão. E também é bom usar quando for trabalhar em arquivo que use pouco HTML
+//bootswatch.com
